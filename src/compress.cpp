e2dfb5d0ee0a756316184b75c6bd0d524449ec75
StrongFlaky <strongli2014@hotmail.com>
2020-05-19 20:36:44 -0700

    Update compress.cpp

--- src/compress.cpp
+++ src/compress.cpp
@@ -19,9 +19,12 @@ void pseudoCompression(const string& inFileName, const string& outFileName) {
     char inputArray;
     myfile.open (inFileName, ios::binary);
     vector <unsigned int> freqs;
+    for(int i = 0; i < 256; i++){
+        freqs.push_back(0);
+    }
 
     while(myfile.get(inputArray)){
-        freqs[inputArray]++;
+        freqs[(byte)inputArray]++;
     }
     HCTree newTree;
     newTree.build(freqs);
